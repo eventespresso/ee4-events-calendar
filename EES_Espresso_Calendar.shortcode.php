a9f3f72172a24d45d61d202896110d0d94c26149
Darren Ethier <darren@roughsmootheng.in>
2018-05-06 20:31:27 -0400

    fix phpcs violations (#4)

--- EES_Espresso_Calendar.shortcode.php
+++ EES_Espresso_Calendar.shortcode.php
@@ -1,83 +1,77 @@
-<?php if ( ! defined( 'EVENT_ESPRESSO_VERSION' )) { exit(); }
+<?php
 /*
- * ------------------------------------------------------------------------
- *
- * Event Espresso
- *
- * Event Registration and Management Plugin for WordPress
- *
- * @ package			Event Espresso
- * @ author			Seth Shoultes
- * @ copyright		(c) 2008-2014 Event Espresso  All Rights Reserved.
- * @ license			http://eventespresso.com/support/terms-conditions/   * see Plugin Licensing *
- * @ link				http://www.eventespresso.com
- * @ version		 	EE4
- *
  * ------------------------------------------------------------------------
  *
  * EE_Calendar
  *
- * @package			Event Espresso
- * @subpackage		espresso-calendar
- * @author			Seth Shoultes, Chris Reynolds, Brent Christensen
+ * @package         Event Espresso
+ * @subpackage       espresso-calendar
+ * @author            Seth Shoultes, Chris Reynolds, Brent Christensen
  *
  * ------------------------------------------------------------------------
  */
-class EES_Espresso_Calendar  extends EES_Shortcode {
+class EES_Espresso_Calendar extends EES_Shortcode
+{
 
 
 
-	/**
-	 * 	set_hooks - for hooking into EE Core, modules, etc
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public static function set_hooks() {
-	}
+
+
+    /**
+     *  set_hooks - for hooking into EE Core, modules, etc
+     *
+     *  @access     public
+     *  @return     void
+     */
+    public static function set_hooks()
+    {
+    }
 
 
 
-	/**
-	 * 	set_hooks_admin - for hooking into EE Admin Core, modules, etc
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public static function set_hooks_admin() {
-	}
+    /**
+     *  set_hooks_admin - for hooking into EE Admin Core, modules, etc
+     *
+     *  @access     public
+     *  @return     void
+     */
+    public static function set_hooks_admin()
+    {
+    }
 
 
 
-	/**
-	 * 	set_definitions
-	 *
-	 *  @access 	public
-	 *  @return 	void
-	 */
-	public static function set_definitions() {
-	}
+    /**
+     *  set_definitions
+     *
+     *  @access     public
+     *  @return     void
+     */
+    public static function set_definitions()
+    {
+    }
 
 
 
-	/**
-	 * 	run - initial shortcode module setup called during "wp_loaded" hook
-	 * 	this method is primarily used for loading resources that will be required by the shortcode when it is actually processed
-	 *
-	 *  @access 	public
-	 *  @param 	 WP $WP
-	 *  @return 	void
-	 */
-	public function run( WP $WP ) {
-		// this will trigger the EED_Espresso_Calendar module's run() method during the pre_get_posts hook point,
-		// this allows us to initialize things, enqueue assets, etc,
-		// as well, this saves an instantiation of the module in an array, using 'calendar' as the key, so that we can retrieve it
-		add_action( 'pre_get_posts', array( EED_Espresso_Calendar::instance(), 'run' ));
-	}
+    /**
+     *  run - initial shortcode module setup called during "wp_loaded" hook
+     *  this method is primarily used for loading resources that will be required by the shortcode when it is actually processed
+     *
+     *  @access     public
+     *  @param   WP $WP
+     *  @return     void
+     */
+    public function run(WP $WP)
+    {
+        // this will trigger the EED_Espresso_Calendar module's run() method during the pre_get_posts hook point,
+        // this allows us to initialize things, enqueue assets, etc,
+        // as well, this saves an instantiation of the module in an array, using 'calendar' as the key, so that we can retrieve it
+        add_action('pre_get_posts', array( EED_Espresso_Calendar::instance(), 'run' ));
+    }
 
 
 
-	/**
+    /**
      * process_shortcode
      *
      * [ESPRESSO_CALENDAR]
@@ -87,17 +81,18 @@ class EES_Espresso_Calendar  extends EES_Shortcode {
      * @access    public
      * @param array $attributes
      * @return string
-	 */
-	public function process_shortcode( $attributes = array() ) {
+     */
+    public function process_shortcode($attributes = array())
+    {
         // make sure $attributes is an array
-		$attributes = array_merge(\EED_Espresso_Calendar::getCalendarDefaults(), (array)$attributes );
-		if (method_exists('\EES_Shortcode', 'sanitize_attributes')) {
+        $attributes = array_merge(\EED_Espresso_Calendar::getCalendarDefaults(), (array) $attributes);
+        if (method_exists('\EES_Shortcode', 'sanitize_attributes')) {
             $attributes = \EES_Shortcode::sanitize_attributes($attributes);
         } else {
             $attributes = $this->sanitize_the_attributes($attributes);
         }
-        return EED_Espresso_Calendar::instance()->display_calendar( $attributes );
-	}
+        return EED_Espresso_Calendar::instance()->display_calendar($attributes);
+    }
 
 
 
@@ -112,42 +107,46 @@ class EES_Espresso_Calendar  extends EES_Shortcode {
     private function sanitize_the_attributes(array $attributes, $custom_sanitization = array())
     {
         foreach ($attributes as $key => $value) {
-            // is a custom sanitization callback specified ?
-            if (isset($custom_sanitization[$key])) {
-                $callback = $custom_sanitization[$key];
+// is a custom sanitization callback specified ?
+            if (isset($custom_sanitization[ $key ])) {
+                $callback = $custom_sanitization[ $key ];
                 if ($callback === 'skip_sanitization') {
-                    $attributes[$key] = $value;
+                    $attributes[ $key ] = $value;
                     continue;
-                } else if (function_exists($callback)) {
-                    $attributes[$key] = $callback($value);
+                } elseif (function_exists($callback)) {
+                    $attributes[ $key ] = $callback($value);
                     continue;
                 }
             }
             switch (true) {
-                case $value === null :
-                case is_int($value) :
-                case is_float($value) :
+                case $value === null:
+                case is_int($value):
+                case is_float($value):
                     // typical booleans
-                case in_array($value, array(true, 'true', '1', 'on', 'yes', false, 'false', '0', 'off', 'no'), true) :
-                    $attributes[$key] = $value;
+                
+                case in_array($value, array(true, 'true', '1', 'on', 'yes', false, 'false', '0', 'off', 'no'), true):
+                    $attributes[ $key ] = $value;
+
                     break;
-                case is_string($value) :
-                    $attributes[$key] = sanitize_text_field($value);
+                case is_string($value):
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      $attributes[ $key ] = sanitize_text_field($value);
+
                     break;
-                case is_array($value) :
-                    $attributes[$key] = $this->sanitize_the_attributes($attributes);
+                case is_array($value):
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      $attributes[ $key ] = $this->sanitize_the_attributes($attributes);
+
                     break;
-                default :
+                default:
                     // only remaining data types are Object and Resource
-                    // which are not allowed as shortcode attributes
-                    $attributes[$key] = null;
+                    
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      // which are not allowed as shortcode attributes
+                    $attributes[ $key ] = null;
+
                     break;
             }
         }
         return $attributes;
     }
-
-
 }
 // End of file EES_Espresso_Calendar.shortcode.php
 // Location: /espresso_calendar/EES_Espresso_Calendar.shortcode.php
